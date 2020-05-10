bcb854ac10b87046af4adbada0bf6591543d2c14
mogaming <seiya.orz@gmail.com>
2020-05-10 11:08:08 +0900

    create-next-app with custom server typescript

--- server/index.ts
+++ server/index.ts
@@ -1,30 +1,18 @@
-import { createServer } from 'http'
-import { parse } from 'url'
 import next from 'next'
-
+const express = require('express')
 const port = parseInt(process.env.PORT || '3000', 10)
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
 app.prepare().then(() => {
-  createServer((req, res) => {
-    const parsedUrl = parse(req.url!, true)
-    const { pathname, query } = parsedUrl
+  const server = express()
+  server.get('*', (req: any, res: any) => {
+    return handle(req, res)
+  })
+  server.listen(port, (err: any) => {
+    if (err) throw err
+    console.log(`> Ready on ${process.env.CLIENT_URL || `http://localhost:${port}`}`)
+  })
 
-    if (pathname === '/a') {
-      app.render(req, res, '/a', query)
-    } else if (pathname === '/b') {
-      app.render(req, res, '/b', query)
-    } else {
-      handle(req, res, parsedUrl)
-    }
-  }).listen(port)
-
-  // tslint:disable-next-line:no-console
-  console.log(
-    `> Server listening at http://localhost:${port} as ${
-      dev ? 'development' : process.env.NODE_ENV
-    }`
-  )
 })
