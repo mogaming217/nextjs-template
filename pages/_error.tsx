7282d51d90d8dc3ba355426db9bdf5fdeec2654f
mogaming <seiya.orz@gmail.com>
2020-05-10 11:33:21 +0900

    custom error page

---
+++ pages/_error.tsx
@@ -0,0 +1,23 @@
+import { NextPage, NextPageContext } from 'next'
+
+type Props = {
+  statusCode: number,
+  message?: string
+}
+
+const ErrorPage: NextPage<Props> = (props: Props) => {
+  return (
+    <div>
+      <div>{ props.statusCode }</div>
+    </div>
+  )
+}
+
+
+ErrorPage.getInitialProps = ({ res, err }: NextPageContext): Props => {
+  const statusCode: number = res?.statusCode || err?.statusCode || 404
+  const message = err?.message
+  return { statusCode, message }
+}
+
+export default ErrorPage
