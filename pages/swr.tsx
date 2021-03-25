f2782c7cdff8f17100e042e3048111c14cc1b548
mogaming <seiya.orz@gmail.com>
2021-03-25 09:47:35 +0900

    try swr

---
+++ pages/swr.tsx
@@ -0,0 +1,25 @@
+import { NextPage, GetServerSideProps, GetServerSidePropsResult } from 'next'
+
+type Props = {
+  currentTime: string
+}
+
+const Page: NextPage<Props> = (props: Props) => {
+  return <p>{props.currentTime}</p>
+}
+
+export const getServerSideProps: GetServerSideProps = ({ res }): Promise<GetServerSidePropsResult<Props>> => {
+  const date = new Date()
+  const currentTime = date.toLocaleString()
+
+  // 👇 ここがポイント
+  res.setHeader('Cache-Control', 'public, s-maxage=10, stale-while-revalidate=86400')
+
+  return Promise.resolve({
+    props: {
+      currentTime,
+    },
+  })
+}
+
+export default Page
