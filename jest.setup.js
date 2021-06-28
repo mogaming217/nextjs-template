d3fe82a19549478b08bc1a94960ffe7dd45d169e
mogaming <seiya.orz@gmail.com>
2021-06-28 15:32:16 +0900

    test: use firestore and import logic

---
+++ jest.setup.js
@@ -0,0 +1,5 @@
+const firebase = require('firebase/app')
+firebase.initializeApp({
+  apiKey: 'sample',
+  projectId: 'fake-project-id',
+})
--- src/test/sample.test.ts
+++ src/test/sample.test.ts
@@ -1,10 +1,3 @@
-// organize-imports-ignore
-import firebase from 'firebase/app'
-firebase.initializeApp({
-  apiKey: 'sample',
-  projectId: 'fake-project-id',
-})
-
 import { getUser } from 'logic/sample'
 
 describe('firestore', () => {
